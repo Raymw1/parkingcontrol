614468f4ce5491bcb2fa9aa72619820a001d1808
Rayan <rayan.rmw@gmail.com>
2022-11-28 16:32:35 -0300

    Add save method on parkingSpot service

--- src/main/java/com/api/parkingcontrol/services/ParkingSpotService.java
+++ src/main/java/com/api/parkingcontrol/services/ParkingSpotService.java
@@ -3,12 +3,18 @@ package com.api.parkingcontrol.services;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.api.parkingcontrol.models.ParkingSpot;
 import com.api.parkingcontrol.repositories.ParkingSpotRepository;
+
+import jakarta.transaction.Transactional;
 
 @Service
 public class ParkingSpotService {
 	@Autowired
 	ParkingSpotRepository parkingSpotRepository;
-	
-	
+
+	@Transactional
+	public ParkingSpot save(ParkingSpot parkingSpotModel) {
+		return parkingSpotRepository.save(parkingSpotModel);
+	}
 }
