bdf9afe52a771bfd18ef506da19984795200ec45
Rayan <rayan.rmw@gmail.com>
2022-11-28 17:34:57 -0300

    Add pagination on findAll method for parkingSpot

--- src/main/java/com/api/parkingcontrol/services/ParkingSpotService.java
+++ src/main/java/com/api/parkingcontrol/services/ParkingSpotService.java
@@ -5,6 +5,8 @@ import java.util.Optional;
 import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import com.api.parkingcontrol.models.ParkingSpot;
@@ -34,8 +36,8 @@ public class ParkingSpotService {
 		return parkingSpotRepository.existsByApartmentAndBlock(apartment, block);
 	}
 
-	public List<ParkingSpot> findAll() {
-		return parkingSpotRepository.findAll();
+	public Page<ParkingSpot> findAll(Pageable pageable) {
+		return parkingSpotRepository.findAll(pageable);
 	}
 
 	public Optional<ParkingSpot> findById(UUID id) {
