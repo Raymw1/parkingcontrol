d749c351d723823f3b25a4a81b87ff4663a2488b
Rayan <rayan.rmw@gmail.com>
2022-11-28 17:04:43 -0300

    Add findOne method for parkingSpot

--- src/main/java/com/api/parkingcontrol/services/ParkingSpotService.java
+++ src/main/java/com/api/parkingcontrol/services/ParkingSpotService.java
@@ -1,6 +1,8 @@
 package com.api.parkingcontrol.services;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -34,5 +36,9 @@ public class ParkingSpotService {
 
 	public List<ParkingSpot> findAll() {
 		return parkingSpotRepository.findAll();
+	}
+
+	public Optional<ParkingSpot> findById(UUID id) {
+		return parkingSpotRepository.findById(id);
 	}
 }
